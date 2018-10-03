654f2dc5d8cbff36f22ad85b025f29135183f1e8
seveniruby <seveniruby@gmail.com>
2018-10-03 23:29:00 +0800

    update bashgems_install with git on linux which without unzip

--- bin/bashgems.sh
+++ bin/bashgems.sh
@@ -11,47 +11,53 @@
 #so you can use it in your functions for parameters parsing
 #todo: add help method
 pp() {
-	local kkk vvv count=$# i
-	for ((i = 1; i <= $count; i++)); do
-		[ -z "$1" ] && shift && continue
-		kkk="${1:1}"
-		if [ "${1:0:1}" = '-' -a "$1" == -[a-zA-Z]* ]; then
-			eval "$kkk=\"${2//\"/\\\"}\"" #unset the var and set value to it
-		fi
-		shift
-	done
+    local kkk vvv count=$# i
+    for ((i = 1; i <= $count; i++)); do
+        [ -z "$1" ] && shift && continue
+        kkk="${1:1}"
+        if [ "${1:0:1}" = '-' -a "$1" == -[a-zA-Z]* ]; then
+            eval "$kkk=\"${2//\"/\\\"}\"" #unset the var and set value to it
+        fi
+        shift
+    done
 }
 
 bashgems_install() {
-	local date=$(date +%Y%m%d)
-	[ -f /tmp/bashgems_$date.zip ] || wget https://github.com/seveniruby/bashgems/archive/master.zip -O /tmp/bashgems_$date.zip
-	unzip -o /tmp/bashgems_$date.zip -d /tmp/bashgems_$date
-	[ -d ~/.bashgems ] || mkdir ~/.bashgems
-	find /tmp/bashgems_$date/bashgems-master -maxdepth 1 -mindepth 1 -exec cp -rf {} ~/.bashgems/ \;
-	grep bashgems.sh ~/.bash_profile || echo '[ -f  ~/.bashgems/bin/bashgems.sh ] && . ~/.bashgems/bin/bashgems.sh' >> ~/.bash_profile
-	. ~/.bash_profile
-	[ -n "$BASHGEMS_HOME" ] && echo success
+    local date=$(date +%Y%m%d)
+    [ -d ~/.bashgems ] || mkdir ~/.bashgems
+    [ -f /tmp/bashgems_$date.zip ] || wget https://github.com/seveniruby/bashgems/archive/master.zip -O /tmp/bashgems_$date.zip
+    if which unzip 2>/dev/null; then
+        unzip -o /tmp/bashgems_$date.zip -d /tmp/bashgems_$date
+        find /tmp/bashgems_$date/bashgems-master -maxdepth 1 -mindepth 1 -exec cp -rf {} ~/.bashgems/ \;
+    elif which git; then
+        git clone https://github.com/seveniruby/bashgems.git ~/.bashgems
+    else
+        echo "you should install unzip or git"
+        return 1
+    fi
+    grep bashgems.sh ~/.bash_profile || echo '[ -f  ~/.bashgems/bin/bashgems.sh ] && . ~/.bashgems/bin/bashgems.sh' >>~/.bash_profile
+    . ~/.bash_profile
+    [ -n "$BASHGEMS_HOME" ] && echo success
 }
 
 logo() {
-	echo '
+    echo '
 #     __            __            __
 #    / /____  _____/ /____  _____/ /_  ____  ____ ___  ___
 #   / __/ _ \/ ___/ __/ _ \/ ___/ __ \/ __ \/ __ `__ \/ _ \
 #  / /_/  __(__  ) /_/  __/ /  / / / / /_/ / / / / / /  __/
 #  \__/\___/____/\__/\___/_/  /_/ /_/\____/_/ /_/ /_/\___/
-#
-    '
+'
 }
 bashgems_init() {
-	logo
-	echo "TesterHome: https://testerhome.com"
-	echo "TTF: https://testerhome.com/topics/15522"
-	echo "BashGems: https://github.com/seveniruby/bashgems.git"
-	echo 
-	[ -f $BASHGEMS_HOME/lib/shellex.sh ] && . $BASHGEMS_HOME/lib/shellex.sh
-	[ -f $BASHGEMS_HOME/lib/hogwarts.sh ] && . $BASHGEMS_HOME/lib/hogwarts.sh
-	:
+    logo
+    echo "TesterHome: https://testerhome.com"
+    echo "TTF: https://testerhome.com/topics/15522"
+    echo "BashGems: https://github.com/seveniruby/bashgems.git"
+    echo
+    [ -f $BASHGEMS_HOME/lib/shellex.sh ] && . $BASHGEMS_HOME/lib/shellex.sh
+    [ -f $BASHGEMS_HOME/lib/hogwarts.sh ] && . $BASHGEMS_HOME/lib/hogwarts.sh
+    :
 }
 
 #########################
--- lib/hogwarts.sh
+++ lib/hogwarts.sh
@@ -6,12 +6,12 @@ hogwarts() {
 #   / / / / /_/ / /_/ /| |/ |/ / /_/ / /  / /_(__  ) 
 #  /_/ /_/\____/\__, / |__/|__/\__,_/_/   \__/____/  
 #              /____/                                
-  '
+'
 	echo "Hogwarts Testing Kit"
 	echo
 
 	if [ "$1" = 'help' ]; then
-		grep "()" $BASHGEMS_HOME/lib/hogwarts.sh
+		grep "() *{" $BASHGEMS_HOME/lib/hogwarts.sh
 	fi
 
 }
@@ -23,9 +23,26 @@ proxy() {
 	http_proxy=http://fq.testerhome.com:$(date +6%m%d) https_proxy=$http_proxy "$@"
 }
 
-chromedriver_download() {
+chromedriver_list() {
 	curl https://raw.githubusercontent.com/appium/appium/master/docs/en/writing-running-appium/web/chromedriver.md 2>/dev/null | grep "|" | grep -v "2.[0-9] " | grep -v "2.1[0-9]"
 	echo chromedriver in CN: https://npm.taobao.org/mirrors/chromedriver
 	echo chromedriver in US: http://chromedriver.storage.googleapis.com/
 	echo chromedriver vs chrome version: https://github.com/appium/appium/blob/master/docs/en/writing-running-appium/web/chromedriver.md
+}
+
+hogwarts_install_stf() {
+	brew install rethinkdb graphicsmagick zeromq protobuf yasm pkg-config
+	npm install -g stf
+}
+
+Hogwarts_start_stf() {
+	rethinkdb &
+	stf local
+}
+
+hogwarts_install_jenkins() {
+	docker run --name jenkins_hogwarts \
+		-p 8080:8080 -p 50000:50000 \
+		-v $PWD/jenkins/data:/var/jenkins_home \
+		registry.docker-cn.com/jenkins/jenkins:lts
 }
