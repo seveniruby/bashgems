ca2fc4adfdd8f578b7717afb2e70ab294916d53d
seveniruby <seveniruby@gmail.com>
2018-10-09 01:20:48 +0800

    add appium install

--- lib/hogwarts.sh
+++ lib/hogwarts.sh
@@ -26,8 +26,8 @@ hogwarts() {
 #use some command with proxy
 #proxy npm install -g appium
 proxy() {
-	local http_proxy=""
-	http_proxy=http://fq.testerhome.com:$(date +6%m%d) https_proxy=$http_proxy "$@"
+	local http_proxy https_proxy
+	http_proxy=http://fq.testing-studio.com:$(date +6%m%d) https_proxy=$http_proxy "$@"
 }
 
 chromedriver_list() {
@@ -51,8 +51,40 @@ Hogwarts_start_stf() {
 }
 
 hogwarts_install_jenkins() {
+	local image=registry.docker-cn.com/jenkins/jenkins:lts
+	docker run -ti --rm --entrypoint="/bin/bash" $image -c "whoami && id"
+	echo "maybe you need chown -R 1000 ~/jenkins/hogwarts"
 	docker run --name jenkins_hogwarts \
 		-p 8080:8080 -p 50000:50000 \
-		-v $PWD/jenkins/data:/var/jenkins_home \
-		registry.docker-cn.com/jenkins/jenkins:lts
+		-v ~/jenkins/hogwarts:/var/jenkins_home \
+		$image
+	echo "you need use such password for init"
+	docker exec jenkins_hogwarts sh -c 'cat /var/jenkins_home/secrets/initialAdminPassword'
+}
+
+hogwarts_install_appium() {
+	if which node; then
+		npm version
+	else
+		echo you should install node 8 or above
+		if which brew; then
+			brew install node
+		else
+			echo you should install brew by /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+		fi
+	fi
+	which java || echo you should install java
+	which adb || echo you should install android sdk and put adb directory in your PATH
+	echo search appium versions
+	proxy npm view appium versions --json | tail
+	echo start install appium
+	proxy npm install -g appium --verbose
+	echo start install ios testing toolkit
+	if which brew; then
+		brew info libimobiledevice || brew install libimobiledevice
+		brew info ios-webkit-debug-proxy || brew install ios-webkit-debug-proxy
+	fi
+	echo start install ios-deploy
+	npm info ios-deploy || npm install -g ios-deploy --verbose
+    appium --version
 }
